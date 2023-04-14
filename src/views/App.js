e225fe46fea43e21de0cc857c3ec96aa0bfed6db
abc <nguyenthanhcong.dn.cv@gmail.com>
2023-04-14 16:35:55 +0700

    Bai # 4

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
 
+import MyComponent from './Example/MyComponent.js'
+
+
+
+
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +23,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <MyComponent/>
+
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render (){
+        return (
+            <h3>Hello first component</h3>
+        )
+    }
+}
+
+export default MyComponent;
