9c26ad71d8fd1b7bb8439188f1454f612c65d0c6
abc <nguyenthanhcong.dn.cv@gmail.com>
2023-04-15 07:16:09 +0700

    #5

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {Fragment} from "react";
+
+
+
+/**
+ * <> </> Fragement kiểu mới
+ */
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Success',
+        job: 'Developer',
+    };
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+
+
     render (){
+
+        // let name = "Thành Công";
+
         return (
-            <h3>Hello first component</h3>
+            <> 
+                {/* {console.log("(1)The name is", name)} */}
+                <div className="first">
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)} />
+                    Hello first component, my name is {this.state.name}
+                </div>
+
+                <div>
+                    Hello second component, my job is {this.state['job']}
+                </div>
+            </>
         )
     }
 }
