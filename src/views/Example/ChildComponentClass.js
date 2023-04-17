a0dabadac4603fe2077a01387b7d23387a508ebe
abc <nguyenthanhcong.dn.cv@gmail.com>
2023-04-17 19:11:36 +0700

    #14

--- src/views/Example/ChildComponentClass.js
+++ src/views/Example/ChildComponentClass.js
@@ -5,28 +5,53 @@ import React, { Fragment } from "react";
  */
 
 class ChildComponentClass extends React.Component {
+  state = {
+    showJob: false,
+  };
+
+  handleShowHide = () => {
+    this.setState({ showJob: !this.state.showJob });
+  };
+
   render() {
     let { arrJobProps } = this.props;
+    let { showJob } = this.state;
 
     return (
       <>
-        <div>
-          <button>Show</button>
-        </div>
-
-        <div className="job-list">
-          {arrJobProps.map((item, index) => {
-            return (
-              <div key={item.id}>
-                {item.jobTitle} - {item.salary}
-              </div>
-            );
-          })}
-        </div>
-
-        <div>
-          <button>Hide</button>
-        </div>
+        {showJob === false ? ( //true condition (condition?true:false)
+          <div>
+            <button
+              onClick={() => {
+                this.handleShowHide();
+              }}
+            >
+              Show
+            </button>
+          </div>
+        ) : (
+          // false condition
+          <>
+            <div className="job-list">
+              {arrJobProps.map((item, index) => {
+                return (
+                  <div key={item.id}>
+                    {item.jobTitle} - {item.salary}
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              <button
+                onClick={() => {
+                  this.handleShowHide();
+                }}
+              >
+                Hide
+              </button>
+            </div>
+          </>
+        )}
       </>
     );
   }
