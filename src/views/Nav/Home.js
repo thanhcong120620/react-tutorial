505b05e8a6a0d7af140b89c5bf70dac3d87b686e
thanhcong120620 <nguyenthanhcong.dn.cv@gmail.com>
2023-05-07 17:46:05 +0700

    #29

--- src/views/Nav/Home.js
+++ src/views/Nav/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 // import { withRouter } from "react-router";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
+import home from "../../assets/images/home.png";
+
 class Home extends React.Component {
   componentDidMount() {
     setTimeout(() => {
       // console.log("Check Timeout");
-      this.props.navigate("/todo");
+      // this.props.navigate("/todo");
     }, 1000);
   }
 
@@ -14,6 +16,13 @@ class Home extends React.Component {
     // console.log(">>>Check props: ", this.props);
     return (
       <>
+        <img
+          src={home}
+          className="Home-logo"
+          alt="logo"
+          width="150px"
+          height="150px"
+        />
         <h2>Hello from Home</h2>
       </>
     );
